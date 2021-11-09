babb37fe574ef8eca9f6a14e71ca4f63ed289fbe
Danish64 <danishali0051@gmail.com>
2021-11-09 17:57:00 +0500

    eslint configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'airbnb', 'airbnb-typescript',
+  'airbnb/hooks',
+  'plugin:@typescript-eslint/recommended',
+  'plugin:@typescript-eslint/recommended-requiring-type-checking',],
+  parserOptions: {
+     project: './tsconfig.eslint.json'
+     },
 };
